ad72416189fef16a4b346f87407bbef6796b04c7
Patrícia Canossa <p.canossa@hotmail.com>
2025-07-15 12:20:29 -0300

    Versão de tentaiva de compatibilidade keystore exigida para build em Android. Excluido node.modeules e package-lock.json, e realizada novo carregamento (npm install). Problema ainda persiste, mas nova tentaiva será realizada. Asendo contruído normal no Android Studio.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'Vetcalc',
-  webDir: 'www'
+  webDir: 'www',
+  android: {
+    signing: {
+      "keystorePath": "vetcalcbr.jks",
+      "keystorePassword": "vetcalcbr",
+      "keystoreKeyAlias": "vetcalc",
+      "keystoreKeyPassword": "Sp1ce7re*"
+    }
+  }
 };
 
 export default config;
